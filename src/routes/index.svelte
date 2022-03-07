c9d0ba864e9de167fc8a269bc0df53df9957d5e3
Rhys <36913739+ryceg@users.noreply.github.com>
2022-03-08 10:51:42 +1100

    test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,12 @@
-<script>
-	import { addSVG, defaultParams, doMap } from '$lib/terrain';
+<script lang="ts">
+	import { defaultParams, doMap } from '$lib/terrain';
 	import { onMount } from 'svelte';
+
 	onMount(() => {
-		doMap(addSVG('final'), defaultParams);
+		doMap('final', defaultParams);
 	});
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<div id="final" />
+<svg id="final" />
