f1b057ae3f9cfa44486bf760adbbf91d4519d4cd
Rhys <36913739+ryceg@users.noreply.github.com>
2022-06-18 13:33:05 +1000

    rename to be more descriptive

--- src/lib/doErosion.ts
+++ src/lib/doErosion.ts
@@ -3,9 +3,9 @@ import { erode } from './erode';
 import { fillSinks } from './fillSinks';
 import type { HInterface } from './hinterface';
 
-export function doErosion(h: HInterface, amount: number, n = 1) {
+export function doErosion(h: HInterface, amount: number, iterations = 1) {
   h = fillSinks(h);
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < iterations; i++) {
     h = erode(h, amount);
     h = fillSinks(h);
   }
